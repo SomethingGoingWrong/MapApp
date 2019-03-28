0b489aeaa8cd91ca97a19f326397311b2eb5838b
Nazar Voytovich <twinterrory@gmail.com>
2019-03-28 17:10:00 +0200

    Initial Commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Назар on 3/28/19.
+//  Copyright © 2019 Nazar Voytovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
